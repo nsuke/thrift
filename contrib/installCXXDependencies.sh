55e76025bbce14c46cc3c233ceab220a7eae7fc3
Nobuaki Sukegawa <nsukeg@gmail.com>
2014-12-08 00:57:21 +0900

    Try with ppa Qt4

--- contrib/installCXXDependencies.sh
+++ contrib/installCXXDependencies.sh
@@ -23,12 +23,12 @@
 
 
 # General dependencies
-# sudo add-apt-repository ppa:ubuntu-sdk-team/ppa -y
-# sudo apt-get update -qq
-# sudo apt-get install -qq qtbase5-dev qtbase5-dev-tools qt5-default
+sudo add-apt-repository ppa:ubuntu-sdk-team/ppa -y
+sudo apt-get update -qq
+sudo apt-get install -qq libqt4-dev qtbase5-dev qtbase5-dev-tools qt5-default
 
 sudo apt-add-repository "deb http://archive.ubuntu.com/ubuntu/ trusty main restricted universe" -y
 sudo apt-get update -qq
-sudo apt-get install -qq libboost-dev libboost-test-dev libboost-program-options-dev libboost-system-dev libboost-filesystem-dev libevent-dev automake libtool flex bison pkg-config g++ libssl-dev make libqt4-dev git debhelper bc qt5-default qt5-qmake qtbase5-dev-tools qtchooser qtbase5-dev
+sudo apt-get install -qq libboost-dev libboost-test-dev libboost-program-options-dev libboost-system-dev libboost-filesystem-dev libevent-dev automake libtool flex bison pkg-config g++ libssl-dev make git debhelper bc
 
 dpkg -S /usr/include/boost/version.hpp
