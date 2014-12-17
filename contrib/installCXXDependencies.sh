4491724a48e1a5b0eb2ac773f28d0d248f81dbae
Nobuaki Sukegawa <nsukeg@gmail.com>
2014-12-17 16:02:34 +0900

    Qt5

--- contrib/installCXXDependencies.sh
+++ contrib/installCXXDependencies.sh
@@ -23,7 +23,11 @@
 
 
 # General dependencies
+sudo add-apt-repository ppa:ubuntu-sdk-team/ppa -y
+sudo apt-get update -qq
+sudo apt-get install -qq libqt4-dev qtbase5-dev qtbase5-dev-tools qt5-default
+
 sudo apt-add-repository "deb http://archive.ubuntu.com/ubuntu/ trusty main restricted" -y
 sudo apt-get update -qq
-sudo apt-get install -qq libboost-dev libboost-test-dev libboost-program-options-dev libboost-system-dev libboost-filesystem-dev libboost-thread-dev libevent-dev automake libtool flex bison pkg-config g++ libssl-dev make libqt4-dev git debhelper bc
+sudo apt-get install -qq libboost-dev libboost-test-dev libboost-program-options-dev libboost-system-dev libboost-filesystem-dev libboost-thread-dev libevent-dev automake libtool flex bison pkg-config g++ libssl-dev make git debhelper bc
 dpkg -S /usr/include/boost/version.hpp
